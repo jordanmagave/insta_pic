027ff87047b5a46e83c0efedb8c3e5b738f52621
Jordan <jordan.magave@gmail.com>
2021-11-05 09:02:59 -0300

    config mkdir

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from instalooter.looters import ProfileLooter
 
@@ -7,6 +9,10 @@ app = FastAPI()
 @app.get("/{username}")
 async def root(username):
     looter = ProfileLooter(str(username))
+
     directory = str('pic_'+username)
+    parent_dir = "home/User/Downloads"
+    path = os.path.join(parent_dir, directory)
+    os.mkdir(path)
+    
     looter.download_pictures(directory, media_count=50)
-    return 'Download Successful'
